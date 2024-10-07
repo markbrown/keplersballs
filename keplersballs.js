da238af0bb5120f7008903440765451d858b99b0
Mark Brown <mark@mercurylang.org>
2024-10-07 21:45:28 +1100

    draw a planet

--- keplersballs.js
+++ keplersballs.js
@@ -1,4 +1,6 @@
-function World() {
+const TAU = 2 * Math.PI;
+
+function World(color = "yellow", radius = 10) {
     this.canvas = document.querySelector("#canvas");
     this.width = canvas.width = window.innerWidth;
     this.height = canvas.height = window.innerHeight;
@@ -7,6 +9,9 @@ function World() {
     this.ctx = this.canvas.getContext("2d");
     this.ctx.translate(this.width / 2, this.height / 2);
     this.ctx.scale(1, -1);
+
+    this.color = color;
+    this.radius = radius;
 }
 
 World.prototype.clear = function() {
@@ -15,8 +20,32 @@ World.prototype.clear = function() {
         this.width, this.height);
 }
 
+World.prototype.draw = function(clear = true) {
+    if (clear) {
+        this.clear();
+    }
+    Vec().spot(this.ctx, this.radius, this.color);
+}
+
+function Vec(x = 0, y = 0) {
+    if (!new.target) {
+        return new Vec(x, y);
+    }
+    this.x = x;
+    this.y = y;
+}
+
+Vec.prototype.spot = function(ctx, radius, color = null) {
+    if (color) {
+        ctx.fillStyle = color;
+    }
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, radius, 0, TAU);
+    ctx.fill();
+}
+
 const w = new World();
 
-w.clear();
+w.draw();
 
 // vi: set ai sw=4 ts=8 et sts=4 :
